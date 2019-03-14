b7ec4e26dcacce6fdff15d4902f86831aa33c237
AndreTGMello <andre.tgmello@gmail.com>
2019-03-14 17:56:40 -0300

    Reduced used to center at minimum.

--- contour_plots.jl
+++ contour_plots.jl
@@ -1,9 +1,14 @@
-loss = soft_l1
+linear(x) = x
+#loss = soft_l1
+loss = linear
+desired_precision = Float64
 
-function contour_3d_plots(x, y, z, phi; title="")
+function contour_3d_plots(x, y, z, phi; title="", reduced=false)
     cont = contour(x, y, z, fill=true, title=title)
-    vline!(cont, [phi[1]])
-    hline!(cont, [phi[2]])
+    if reduced == false
+        vline!(cont, [phi[1]])
+        hline!(cont, [phi[2]])
+    end
     display(cont)
 
     three_dim = surface(x,y,z, cbar=true, title=title)
@@ -11,7 +16,7 @@ function contour_3d_plots(x, y, z, phi; title="")
 end
 
 
-function calc_z_plot(x, y, data, ode_fun, phi, t; fun="am")
+function calc_z_plot(x, y, data, ode_fun, phi, t; fun="am", reduced=false)
     z = desired_precision[]
     if fun=="am"
         obj_fun = adams_moulton_estimator
@@ -22,25 +27,33 @@ function calc_z_plot(x, y, data, ode_fun, phi, t; fun="am")
     for i in range(1, stop=length(x))
         for j in range(1, stop=length(y))
             phi_eval = [x[i], y[j]]
-            obj_eval = sum(loss.(abs2.(obj_fun(phi_eval, data, t, ode_fun))))
+            obj_eval = sum(loss.(abs2.(obj_fun(convert(Array{desired_precision}, phi_eval),
+                                convert(Array{desired_precision}, data),
+                                convert(Array{desired_precision}, t),
+                                ode_fun))))
             push!(z, obj_eval)
+            #println("$i,$j\nPhi: $phi_eval\nRes: $obj_eval")
         end
     end
     z = reshape(z, (length(y), length(x)))
     num_samples = length(data)
-    contour_3d_plots(x, y, z, phi, title="$fun for num_samples=$num_samples")
+    contour_3d_plots(x, y, z, phi, title="$fun for num_samples=$num_samples", reduced=reduced)
+
+    a = findmin(z)
+    return a
 end
 
-for i in [1,4,6]
+for i in [6]
     println("\n----- Plots for problem $i -----\n")
-    ode_fun = ode_fun_array[i]
-    t = t_array[i]
-    phi = phi_array[i]
-    bounds = bounds_array[i]
-    ini_cond = ini_cond_array[i]
-    rand_range = rand_range_array[i]
-    min_range = rand_range[1]
-    max_range = rand_range[end]
+    p_solve = problem_set[i]
+    ode_fun = p_solve.fun
+    t = p_solve.t
+    phi = p_solve.phi
+    bounds = p_solve.bounds
+    ini_cond = p_solve.data[:,1]
+    rand_range = p_solve.bounds
+    min_range = rand_range[1][1]
+    max_range = rand_range[end][end]
     delta_t = desired_precision(.1)
 
     for num_samples in 3:20:23
@@ -50,46 +63,23 @@ for i in [1,4,6]
         saveat_t = t[1]:((t[end]-t[1])/num_samples):t[end]
         osol  = solve(oprob, Tsit5(), saveat=saveat_t)
         plot(osol)
-        println(osol.t)
-        println(osol.u)
         data = reduce(hcat, osol.u)
-        println(data)
         data_plot = scatter(saveat_t, transpose(data))
         display(data_plot)
 
         x = range(min_range, max_range, step=.05)
         y = range(min_range, max_range, step=.05)
 
-        x_reduced = range(phi[1]-.05, phi[1]+.05, step=.001)
-        y_reduced = range(phi[2]-.05, phi[2]+.05, step=.001)
-
         println("\n----- Adams-Moulton Estimator -----")
-        calc_z_plot(x, y, data, ode_fun, phi, saveat_t, fun="am")
-        calc_z_plot(x_reduced, y_reduced, data, ode_fun, phi, saveat_t, fun="am")
+        f_min,f_min_idx = calc_z_plot(x, y, data, ode_fun, phi, saveat_t, fun="am")
+        x_reduced = range(x[f_min_idx[2]]-.5, x[f_min_idx[2]]+.5, step=.05)
+        y_reduced = range(y[f_min_idx[1]]-.5, y[f_min_idx[1]]+.5, step=.05)
+        calc_z_plot(x_reduced, y_reduced, data, ode_fun, phi, saveat_t, fun="am", reduced=true)
 
         println("\n----- Classic Estimator -----")
-        calc_z_plot(x, y, data, ode_fun, phi, saveat_t, fun="cla")
-        calc_z_plot(x_reduced, y_reduced, data, ode_fun, phi, saveat_t, fun="cla")
+        f_min,f_min_idx = calc_z_plot(x, y, data, ode_fun, phi, saveat_t, fun="cla")
+        x_reduced = range(x[f_min_idx[2]]-.5, x[f_min_idx[2]]+.5, step=.05)
+        y_reduced = range(y[f_min_idx[1]]-.5, y[f_min_idx[1]]+.5, step=.05)
+        calc_z_plot(x_reduced, y_reduced, data, ode_fun, phi, saveat_t, fun="cla", reduced=true)
     end
 end
-
-x = range(1, 10, step=1)
-y = range(1, 4, step=1)
-
-z = []
-for i in x
-    for j in y
-        push!(z, i*j)
-    end
-end
-cont = contour(x, y, z, aspect_ratio=1, fill=true)
-print(z)
-z = reshape(z,(length(y),length(x)))
-cont = contour(x, y, z, aspect_ratio=1, fill=true)
-vline!(cont, [3])
-hline!(cont, [3])
-print(z)
-
-z = float((1:4) * reshape(1:10, 1, :))
-cont = contour(x, y, z, aspect_ratio=1, fill=true)
-print(z)
