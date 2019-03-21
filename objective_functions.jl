417ea82db5a4d980f5ccd271f42c37aa4ac29ceb
AndreTGMello <andre.tgmello@gmail.com>
2019-03-21 17:23:02 -0300

    Adams-Moulton Fourth Order.

--- objective_functions.jl
+++ objective_functions.jl
@@ -20,8 +20,64 @@ function adams_moulton_estimator(phi, data, time_array, ode_fun; plot_estimated=
     end
 
     if plot_estimated
-        p = scatter(transpose(estimated))
-        display(p)
+        p_data = scatter(transpose(estimated))
+        scatter!(p_data, transpose(data))
+        display(p_data)
+        println("Plot for\n$phi\n")
+    end
+
+    weight = abs2(1/findmax(data)[1])
+    residuals = weight .* (data-estimated)
+    #=>
+    if findmin(estimated)[1] < 0
+        for i in 1:length(residuals)
+            residuals[i] = 10^10
+        end
+    end
+    <=#
+    return residuals
+end
+
+function adams_moulton_fourth_estimator(phi, data, time_array, ode_fun; plot_estimated=false)
+    num_state_variables, num_samples = size(data)
+
+    estimated = zeros(promote_type(eltype(phi),eltype(data)), num_samples*num_state_variables)
+    estimated = reshape(estimated, (num_state_variables, num_samples))
+    estimated[:, 1] = data[:,1] #Initial conditions are stored at x_dot_num's first column
+    estimated[:, 2] = data[:,2] #Initial conditions are stored at x_dot_num's first column
+    estimated[:, 3] = data[:,3] #Initial conditions are stored at x_dot_num's first column
+    estimated[:, 4] = data[:,4] #Initial conditions are stored at x_dot_num's first column
+
+    for i in range(1, stop=num_samples-3)
+        delta_t = []
+        push!(delta_t, time_array[i+1] - time_array[i])
+        push!(delta_t, time_array[i+2] - time_array[i+1])
+        push!(delta_t, time_array[i+3] - time_array[i+2])
+        push!(delta_t, time_array[i+1] - time_array[i])
+
+        x_k = []
+        push!(x_k, data[:, i])
+        push!(x_k, data[:, i+1])
+        push!(x_k, data[:, i+2])
+        push!(x_k, data[:, i+3])
+
+        f_eval_1 = zeros(promote_type(eltype(phi),eltype(data)), num_state_variables)
+        ode_fun(f_eval_1, x_k[1], phi, 0)
+        f_eval_2 = zeros(promote_type(eltype(phi),eltype(data)), num_state_variables)
+        ode_fun(f_eval_2, x_k[2], phi, 0)
+        f_eval_3 = zeros(promote_type(eltype(phi),eltype(data)), num_state_variables)
+        ode_fun(f_eval_3, x_k[3], phi, 0)
+        f_eval_4 = zeros(promote_type(eltype(phi),eltype(data)), num_state_variables)
+        ode_fun(f_eval_4, x_k[4], phi, 0)
+
+        x_k_est = x_k[3] + (delta_t[1]*(9/24)*f_eval_4+delta_t[1]*(19/24)*f_eval_3-delta_t[1]*(5/24)f_eval_2+delta_t[1]*(1/24)*f_eval_1)
+        estimated[:, i+3] = x_k_est
+    end
+
+    if plot_estimated
+        p_data = scatter(transpose(estimated))
+        scatter!(p_data, transpose(data))
+        display(p_data)
         println("Plot for\n$phi\n")
     end
 
@@ -41,7 +97,7 @@ end
 function data_shooting_estimator(phi, data, t, ode_fun; steps=1, plot_estimated=false)
     num_state_variables, num_samples = size(data)
 
-    estimated = zeros(desired_precision, num_samples*num_state_variables)
+    estimated = zeros(promote_type(eltype(phi),eltype(data)), num_samples*num_state_variables)
     estimated = reshape(estimated, (num_state_variables, num_samples))
     estimated[:, 1] = data[:,1] #Initial conditions are stored at x_dot_num's first column
 
@@ -67,11 +123,36 @@ function data_shooting_estimator(phi, data, t, ode_fun; steps=1, plot_estimated=
         p = scatter(transpose(estimated))
         display(p)
         println("Plot for\n$phi\n")
-
     end
 
     residuals = (data-estimated)
     return residuals
+end
+
+function data_shooting_estimator_node(phi, data, t, ode_fun; steps=1)
+    num_state_variables, num_samples  = size(data)
+
+    estimated = zeros(promote_type(eltype(data),eltype(phi)), num_samples*num_state_variables)
+    estimated = reshape(estimated, (num_state_variables, num_samples))
+    estimated[:,1] = data[:,1]
+
+    for i in 1:num_samples-1
+        t_1 = t[i+1]
+        t_0 = t[i]
+        delta_t = (t_1 - t_0)/steps
+
+        x_k_0 = data[:, i]
+
+        x_k_1 = 0
+        for j in 1:steps
+            f_eval = zeros(promote_type(eltype(data),eltype(phi)), num_state_variables)
+            ode_fun(f_eval, x_k_0, phi, 0)
+            x_k_1 = x_k_0 + delta_t.*f_eval
+            x_k_0 = x_k_1
+        end
+        estimated[:, i+1] = x_k_1
+    end
+    estimated
 end
 
 function single_shooting_estimator(phi, data, t, ode_fun; plot_estimated=false)
