3a98438170fb566f516dd5e9e716b8374de57d89
AndreTGMello <andre.tgmello@gmail.com>
2019-03-29 14:57:28 -0300

    Bug fix in the add_noise function.

--- utils.jl
+++ utils.jl
@@ -2,3 +2,21 @@ function rand_guess(bounds)
     [rand(Uniform(bounds[1][i], bounds[end][i]))
         for i in 1:length(bounds[1])]
 end
+
+function add_noise!(data, var)
+    m,n = size(data)
+    for i in 1:m
+        for j in 1:n
+            if data[i,j] != 0.
+                if -var*data[i,j] < var*data[i,j]
+                    a = -var*data[i,j]
+                    b = var*data[i,j]
+                else
+                    b = -var*data[i,j]
+                    a = var*data[i,j]
+                end
+                data[i,j] += rand(Uniform(a, b))
+            end
+        end
+    end
+end
