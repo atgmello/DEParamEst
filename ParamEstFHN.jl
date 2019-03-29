e22e789d090f43b002789af0c28505e892c5d167
AndreTGMello <andre.tgmello@gmail.com>
2019-03-29 17:03:11 -0300

    Changing number of samples. More comments on results.

--- ParamEstFHN.jl
+++ ParamEstFHN.jl
@@ -19,13 +19,13 @@ prob = ODEProblem(fitz, r0, tspan,p)
 tspan2 = (0.0, 3.0)                 # sample of 300 observations with a timestep of 0.01
 prob_short = ODEProblem(fitz, r0, tspan2,p)
 
-dt = 30.0/3000
+dt = 30.0/3
 tf = 30.0
 tinterval = 0:dt:tf
 t  = collect(tinterval)
 
-h = 0.01
-M = 300
+h = 1
+M = 3
 tstart = 0.0
 tstop = tstart + M * h
 tinterval_short = 0:h:tstop
@@ -88,15 +88,17 @@ xtol_rel!(opt,1e-12)
 maxeval!(opt, 10000)
 @time (minf,minx,ret) = NLopt.optimize(opt,glo_init)
 println("\nObjective function:\t$minf\nFor parameters:\n$minx\nReal parameters are:\n$p\nMessage:\t$ret")
+# Very Bad
 
 opt = Opt(:GN_CRS2_LM, 4)
 lower_bounds!(opt,[0.0,0.0,0.0,0.0])
 upper_bounds!(opt,[5.0,5.0,5.0,5.0])
-min_objective!(opt, obj_short.cost_function2)
+min_objective!(opt, obj_short_am)
 xtol_rel!(opt,1e-12)
 maxeval!(opt, 10000)
 @time (minf,minx,ret) = NLopt.optimize(opt,glo_init)
 println("\nObjective function:\t$minf\nFor parameters:\n$minx\nReal parameters are:\n$p\nMessage:\t$ret")
+# Very Good
 
 opt = Opt(:GN_ISRES, 4)
 lower_bounds!(opt,[0.0,0.0,0.0,0.0])
@@ -165,22 +167,22 @@ println("\nObjective function:\t$minf\nFor parameters:\n$minx\nReal parameters a
 opt = Opt(:LN_PRAXIS, 4)
 lower_bounds!(opt,[0.0,0.0,0.0,0.0])
 upper_bounds!(opt,[1.0,1.0,1.0,1.0])
-min_objective!(opt, obj_short.cost_function2)
+min_objective!(opt, obj_short_am)
 xtol_rel!(opt,1e-12)
 maxeval!(opt, 10000)
 @time (minf,minx,ret) = NLopt.optimize(opt,loc_init)
 println("\nObjective function:\t$minf\nFor parameters:\n$minx\nReal parameters are:\n$p\nMessage:\t$ret")
-# Very Good
+# Very Good - Improved Speed Tenfold
 
 opt = Opt(:LN_SBPLX, 4)
 lower_bounds!(opt,[0.0,0.0,0.0,0.0])
 upper_bounds!(opt,[1.0,1.0,1.0,1.0])
-min_objective!(opt, obj_short.cost_function2)
+min_objective!(opt, obj_short_am)
 xtol_rel!(opt,1e-12)
 maxeval!(opt, 10000)
 @time (minf,minx,ret) = NLopt.optimize(opt,loc_init)
 println("\nObjective function:\t$minf\nFor parameters:\n$minx\nReal parameters are:\n$p\nMessage:\t$ret")
-# Very Good
+# Very Good - Improved Speed Tenfold
 
 opt = Opt(:LD_MMA, 4)
 lower_bounds!(opt,[0.0,0.0,0.0,0.0])
@@ -206,7 +208,6 @@ println("Real params are:\n$p")
 
 obj_am(x,y) = sum(abs2.(adams_moulton_estimator(x, data, t, fitz)))
 
-
 opt = Opt(:GN_ORIG_DIRECT_L, 4)
 lower_bounds!(opt,[0.0,0.0,0.0,0.0])
 upper_bounds!(opt,[5.0,5.0,5.0,5.0])
@@ -215,7 +216,7 @@ xtol_rel!(opt,1e-12)
 maxeval!(opt, 10000)
 @time (minf,minx,ret) = NLopt.optimize(opt,glo_init)
 println("\nObjective function:\t$minf\nFor parameters:\n$minx\nReal parameters are:\n$p\nMessage:\t$ret")
-# Very Good - Improved Speed and Estimation
+# Very Good!! - Improved Speed and Estimation CAVEAT: Requires somewhat tight bounds.
 
 opt = Opt(:GN_CRS2_LM, 4)
 lower_bounds!(opt,[0.0,0.0,0.0,0.0])
@@ -251,13 +252,15 @@ println("\nObjective function:\t$minf\nFor parameters:\n$minx\nReal parameters a
 
 opt = Opt(:LN_BOBYQA, 4)
 lower_bounds!(opt,[0.0,0.0,0.0,0.0])
-upper_bounds!(opt,[1.0,1.0,1.0,1.0])
-min_objective!(opt, obj_am)
+upper_bounds!(opt,[10.0,10.0,10.0,10.0])
+min_objective!(opt, obj)
 xtol_rel!(opt,1e-12)
 maxeval!(opt, 10000)
-@time (minf,minx,ret) = NLopt.optimize(opt,loc_init)
+@time (minf,minx,ret) = NLopt.optimize(opt,glo_init)
 println("\nObjective function:\t$minf\nFor parameters:\n$minx\nReal parameters are:\n$p\nMessage:\t$ret")
 # Very Good - Improved convergence: AM gets the same results much faster 1.3 seconds vs 0.3 seconds
+# Converges even from glo_init
+# Both obj and obj_am converge from global_init
 
 opt = Opt(:LN_NELDERMEAD, 4)
 lower_bounds!(opt,[0.0,0.0,0.0,0.0])
@@ -271,28 +274,20 @@ println("\nObjective function:\t$minf\nFor parameters:\n$minx\nReal parameters a
 
 using ForwardDiff
 
-function fitzHugh(du, u, p, t)
-    v,w = u
-    a,b,c,d = p
-    du[1] = v - v^3/3 - w + d
-    du[2] = c*(v + a - b*w)
+obj_am(x,y) = sum(abs2.(adams_moulton_estimator(x, data, t, fitz)))
+obj_am_g(x) = obj_am(x, 0)
+
+function obj_am_grad(x, y)
+    g = z -> ForwardDiff.gradient(obj_am_g,z)
+    aux = g(y)
+    y[:] = aux
+    return obj_am_g(x)
 end
 
-obj_am(x,y) = sum(abs2.(adams_moulton_estimator(x, data, t, fitzHugh)))
-obj_am_g(x) = sum(abs2.(adams_moulton_estimator(x, data, t, fitzHugh)))
-g = z -> ForwardDiff.gradient!(obj_am_g,z)
-g([0.0 0.0 0.0 0.0])
-function obj_am_grad(x, y)
-    println("Got\n$y")
-    g = z -> ForwardDiff.gradient!(obj_am_g,z)
-    g(y)
-    println("Made\n$y")
-    return obj_am(x,y)
-end
 opt = Opt(:LD_SLSQP, 4)
 lower_bounds!(opt,[0.0,0.0,0.0,0.0])
 upper_bounds!(opt,[1.0,1.0,1.0,1.0])
-min_objective!(opt, obj_am_grad)
+min_objective!(opt, obj.cost_function2)
 xtol_rel!(opt,1e-12)
 maxeval!(opt, 10000)
 @time (minf,minx,ret) = NLopt.optimize(opt,loc_init)
