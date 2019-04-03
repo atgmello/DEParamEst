eba82cd17f5d20549ef29dd89cc2d5530af0d241
AndreTGMello <andre.tgmello@gmail.com>
2019-04-03 00:09:46 -0300

    New distance measure (difference between curves).

--- utils.jl
+++ utils.jl
@@ -4,18 +4,34 @@ function rand_guess(bounds)
 end
 
 function add_noise!(data, var)
-    m,n = size(data)
-    for i in 1:m
-        for j in 1:n
-            if data[i,j] != 0.
-                if -var*data[i,j] < var*data[i,j]
-                    a = -var*data[i,j]
-                    b = var*data[i,j]
-                else
-                    b = -var*data[i,j]
-                    a = var*data[i,j]
+    if length(size(data)) == 2
+        m,n = size(data)
+        for i in 1:m
+            for j in 1:n
+                if data[i,j] != 0.
+                    if -var*data[i,j] < var*data[i,j]
+                        a = -var*data[i,j]
+                        b = var*data[i,j]
+                    else
+                        b = -var*data[i,j]
+                        a = var*data[i,j]
+                    end
+                    data[i,j] += rand(Uniform(a, b))
                 end
-                data[i,j] += rand(Uniform(a, b))
+            end
+        end
+    elseif length(size(data)) == 1
+        m = length(data)
+        for i in 1:m
+            if data[i] != 0.
+                if -var*data[i] < var*data[i]
+                    a = -var*data[i]
+                    b = var*data[i]
+                else
+                    b = -var*data[i]
+                    a = var*data[i]
+                end
+                data[i] += rand(Uniform(a, b))
             end
         end
     end
@@ -45,6 +61,31 @@ function mrae(actual::AbstractArray, forecast::AbstractArray)
     n = length(actual)
     e = abs.(actual-forecast)
     stat = (1/n)*sum(e./abs.(actual))
+end
+
+function diff_states(f, u0::AbstractArray, tspan::Tuple, actual::AbstractArray, forecast::AbstractArray; var=1.5)
+    dist = 0.0
+    t = range(tspan[1], stop=tspan[end], length=1000)
+    runs = 50
+    for i in 1:runs
+        u0_perturb = copy(u0)
+        add_noise!(u0_perturb, var)
+        for j in 1:length(u0_perturb)
+            if u0_perturb[j] < 0
+                u0_perturb[j] = 0
+            end
+        end
+        de_prob_actual = ODEProblem(f,u0_perturb,tspan,actual)
+        de_sol_actual = DifferentialEquations.solve(de_prob_actual, AutoVern9(Rodas5()), saveat=t)
+        data_actual = reduce(hcat, de_sol_actual.u)
+
+        de_prob_forecast = ODEProblem(f,u0_perturb,tspan,forecast)
+        de_sol_forecast = solve(de_prob_forecast, AutoVern9(Rodas5()), saveat=t)
+        data_forecast = reduce(hcat, de_sol_forecast.u)
+
+        dist += euclidean(data_actual, data_forecast)/(size(data_actual)[1]*size(data_actual)[2])
+    end
+    dist /= runs
 end
 
 #=>
